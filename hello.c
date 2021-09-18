d90f1fb5402ef03eb1415d15e7edfa4c0901ed84
manoj-38 <90670648+manoj-38@users.noreply.github.com>
2021-09-18 11:13:01 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n"); //getting input from user
+
+    printf("hello, %s\n", name);                      //enter your name
+
+}
